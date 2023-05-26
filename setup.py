d88dfe00dc5c02cac8eb46f20086ecbe6b087bdc
veotos <veotos@gmail.com>
2023-05-26 22:10:03 +0200

    Update setup file with long description

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='veotos_transform',
-      version='0.1',
-      description='The funniest joke in the world',
+      version='0.1.1',
+      description='Obfuscate text with next consonant or vowel',
+      long_description=readme(),
+      classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+      ],
       url='http://github.com/veotos/veotos_aoc',
       author='veotos',
       author_email='veotos@gmail.com',
-      license='GPL',
+      license='GPLv3',
       packages=['veotos_transform'],
+      include_package_data=True,
       zip_safe=False)
